637142dd82c613692ed0f24177c443391b9b6e7b
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-09-29 15:19:17 +0530

    Refactor Sequelize initialization to use process.env variables for improved clarity

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,18 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-
-
-const sequelize = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PASSWORD, {
-  host: MYSQL_HOST,
-  port: MYSQL_PORT,
-  dialect: "mysql",
-  logging: false
-});
-
+const sequelize = new Sequelize(
+  process.env.MYSQL_DB,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    host: process.env.MYSQL_HOST,   // must be "db" in docker
+    port: Number(process.env.MYSQL_PORT) || 3306,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 export default sequelize;
